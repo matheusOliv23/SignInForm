48f5cf04069ba6e24a3535ab1af91650d8f659d3
matheusoliv23 <matheushenri1996@gmail.com>
2022-05-11 20:15:16 -0300

    Finalizando projeto

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const progressSteps = document.querySelectorAll(".progress--step");
 
 let formStepsNum = 0;
 
+
 nextBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
         formStepsNum++;
@@ -62,3 +63,93 @@ function displayContent() {
         content.style.classList.add(".display-content");
     }, 3000);
 }
+
+/* VALIDAÇÃO */
+
+function validarForm() {
+    // pegando o valor do nome pelos names 
+    const nome = document.querySelector("#nome");
+    const email = document.querySelector("#email");
+    const senha = document.getElementById("password");
+    const dominio = document.getElementById("dominio");
+    const telefone = document.getElementById("telefone");
+    const newsletter = document.getElementById("newsletter").checked;
+
+    const errorNome = document.getElementById("errorNome");
+    const errorEmail = document.getElementById("errorEmail");
+    const errorSenha = document.getElementById("errorSenha");
+    const errorDominio = document.getElementById("errorDominio");
+    const errorTelefone = document.getElementById("errorTelefone");
+    const errorNews = document.getElementById("errorNews");
+
+
+    if (nome.value == "" || nome.value.length < 3) {
+        alert('O nome precisa ter pelo menos 3 caracteres')
+        nome.focus();
+        return;
+    }
+
+    if (email.value == "") {
+        alert("Por favor, insira corretamente o seu email");
+        email.focus();
+        alert('email incorreto')
+        return;
+    }
+
+    // if (senha.value == "") {
+    //     errorSenha.innerHTML = "Por favor, insira corretamente o seu email";
+    //     senha.focus();
+    //     alert("email incorreto");
+    //     return;
+    // }
+
+    // const nome = document.getElementById("nome");
+
+    // if (nome.value.length == 0) {
+    //     msgErrp.innerHTML = "O nome deve ser informado";
+    // } else {
+    //     msgErrp.innerHTML = "";
+    // }
+}
+
+/*SELECIONAR NECESSIDADES */
+const rh = document.getElementById("rh");
+const finance = document.getElementById("finance");
+const tele = document.getElementById("tele");
+const juri = document.getElementById("juri");
+const logistic = document.getElementById("logistic");
+const education = document.getElementById("education");
+const health = document.getElementById("health");
+const others = document.getElementById("others");
+
+rh.addEventListener("click", () => {
+    rh.classList.add("add-border-select");
+});
+
+finance.addEventListener("click", () => {
+    finance.classList.add("add-border-select");
+});
+
+tele.addEventListener("click", () => {
+    tele.classList.add("add-border-select");
+});
+
+juri.addEventListener("click", () => {
+    juri.classList.add("add-border-select");
+});
+
+education.addEventListener("click", () => {
+    education.classList.add("add-border-select");
+});
+
+logistic.addEventListener("click", () => {
+    logistic.classList.add("add-border-select");
+});
+
+health.addEventListener("click", () => {
+    health.classList.add("add-border-select");
+});
+
+others.addEventListener("click", () => {
+    others.classList.add("add-border-select");
+});
